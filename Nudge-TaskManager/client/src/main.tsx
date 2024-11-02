97e05ba6be024d8ca2e8221b57efb16dfe1be16e
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2024-11-03 04:20:00 +0800

    implemented login page

---
+++ Nudge-TaskManager/client/src/main.tsx
@@ -0,0 +1,20 @@
+// main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import LoginPage from "./pages/LoginPage";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/LoginPage" element={<LoginPage />} />{" "}
+          {/* Fallback for 404 */}
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  </StrictMode>
+);
