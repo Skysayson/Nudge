6be4909ca6d147fb171f988228c19306ffbce49d
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2024-11-05 00:34:14 +0800

    add: Tasks section

--- Nudge-TaskManager/client/src/components/StatusBar.tsx
+++ Nudge-TaskManager/client/src/components/StatusBar.tsx
@@ -1,9 +1,20 @@
-const StatusBar = ({ Status }: { Status: string }) => {
-    return (
-        <div className="border border-red-600 w-max h-full">
-            <div className="flex border bg-[#192228] items-center p-[15.2px] rounded-xl lg:w-[260px] lg:h-[60px] 2xl:w-[364px] 2xl:h-[65px]">{Status}</div>
-        </div>
-    )
-}
+import TaskCard from "./TaskCard";
+import { StatTask } from ".././interfaces/interfaces";
 
-export default StatusBar
+const StatusBar = ({ TaskStat }: { TaskStat: StatTask }) => {
+  return (
+    <div className="w-full h-full pr-[20px]">
+      <div className="relative bg-[#192228] border-t-[5.056px] border-t-[#88A7BD] p-[15.2px] rounded-xl lg:w-full lg:mr-[10px] lg:h-[60px] shadow-md">
+        <div className="flex items-center">{TaskStat.status}</div>
+      </div>
+
+      <div className="max-h-[75%] overflow-y-auto w-full mt-[10px] scrollbar-hide">
+        {Array.from({ length: TaskStat.tasks }, (_, index) => (
+          <TaskCard key={index} TaskStat={TaskStat} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StatusBar;
