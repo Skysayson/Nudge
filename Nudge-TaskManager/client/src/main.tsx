74798bc8c9769109145dffda3e3e22ca7dc9c3fc
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2024-11-05 00:36:15 +0800

    add: Dashboard route and imported css module

--- Nudge-TaskManager/client/src/main.tsx
+++ Nudge-TaskManager/client/src/main.tsx
@@ -6,15 +6,17 @@ import { MantineProvider } from "@mantine/core";
 import LoginPage from "./pages/LoginPage";
 import BasePage from "./pages/BasePage";
 import "./index.css";
+import DashboardPage from "./pages/DashboardPage";
+import '@mantine/dates/styles.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/DashboardPage" element={<DashboardPage/>}></Route>
           <Route path="/BasePage" element={<BasePage/>}/>
           <Route path="/LoginPage" element={<LoginPage />}/>
-          {/* Fallback for 404 */}
         </Routes>
       </BrowserRouter>
     </MantineProvider>
