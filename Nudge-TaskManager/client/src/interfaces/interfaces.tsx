5e439623a1bc13c29735d986c2f319d3b6f74a81
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2024-11-08 01:18:33 +0800

    change: interface properties for TaskContent to include due date

--- Nudge-TaskManager/client/src/interfaces/interfaces.tsx
+++ Nudge-TaskManager/client/src/interfaces/interfaces.tsx
@@ -1,13 +1,24 @@
 export interface TaskContent {
+    status: string;
     priority: string;
     title: string;
     content: string;
     assigned: string[];
     comments: number;
+    due: Date;
 }
+
+export interface UserTeams {
+    teams: TeamInfo[]
+}
+
+export interface TeamInfo {
+    teamName: string;
+    teamStatTask: StatTask;
+}
+
 export interface StatTask {
     status: string;
-    tasks: number;
-    Content: TaskContent;
+    Task: TaskContent[];
 }
 
