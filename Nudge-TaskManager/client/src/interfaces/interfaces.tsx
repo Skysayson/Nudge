3d8e1576394c45e825b6ae6e9a2136810969cf96
MaryModest'/0/ <20800073@usc.edu.ph>
2024-12-08 20:27:25 +0800

    Add: added in dashboard stuff

--- Nudge-TaskManager/client/src/interfaces/interfaces.tsx
+++ Nudge-TaskManager/client/src/interfaces/interfaces.tsx
@@ -1,24 +1,37 @@
 export interface TaskContent {
-    status: string;
-    priority: string;
-    title: string;
-    content: string;
-    assigned: string[];
-    comments: taskComment[];
-    created: Date;
-    due: Date | null;
+  taskID: number;
+  teamID: number;
+  status: string;
+  priority: string;
+  title: string;
+  content: string;
+  assigned: string[];
+  comments: taskComment[];
+  created: Date | null;
+  due: Date | null;
+}
+
+//THIS ONE IS MINE BTW - USELESS but like just keep it for now typa
+export interface TaskSomething {
+  taskID: number;
+  title: string;
+  description: string;
+  dueDate: Date;
+  priority: string;
+  status: string;
+  teamID: number;
+  createdAt: Date;
 }
 
 export interface taskComment {
-    author: string;
-    comment: string;
-    created: Date;
-    likes: number;
-    dislikes: number;
+  author: string;
+  comment: string;
+  created: Date;
+  likes: number;
+  dislikes: number;
 }
 
 export interface StatTask {
-    status: string;
-    Task: TaskContent[];
+  status: string;
+  Task: TaskContent[];
 }
-
