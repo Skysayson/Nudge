135d3b5d25cfe26f958fb8c174268d2303c259d2
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2025-08-21 03:49:38 +0800

    fix:  sequalize db

--- Nudge-TaskManager/server/models/index.js
+++ Nudge-TaskManager/server/models/index.js
@@ -1,25 +1,47 @@
-const dbConfig = require("../configs/db.config.js");
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
+const dbConfig = require("../configs/db.config");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // use a single connection string from the host (Railway / Render)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: dbConfig.dialect || "mysql",
+    logging: false,
+    dialectOptions: {
+      // uncomment if your DB provider requires SSL
+      // ssl: { require: true, rejectUnauthorized: false }
+    },
+  });
+} else {
+  // fall back to explicit env vars or config defaults
+  sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+      host: dbConfig.HOST,
+      port: dbConfig.port,
+      dialect: dbConfig.dialect,
+      logging: false,
+    }
+  );
+}
 
 // HERE ARE YOUR MODELS
-const User = require("./user/user.model.js")(sequelize, DataTypes);
-const Member = require("./user/member.model.js")(sequelize, DataTypes);
-const Task = require("./team/task.model.js")(sequelize, DataTypes);
-const Team = require("./team/team.model.js")(sequelize, DataTypes);
+const User = require("./user/user.model.js")(sequelize, Sequelize.DataTypes);
+const Member = require("./user/member.model.js")(sequelize, Sequelize.DataTypes);
+const Task = require("./team/task.model.js")(sequelize, Sequelize.DataTypes);
+const Team = require("./team/team.model.js")(sequelize, Sequelize.DataTypes);
 const Notification = require("./notification/notification.model.js")(
   sequelize,
-  DataTypes
+  Sequelize.DataTypes
 );
 const Comment = require("./notification/comment.model.js")(
   sequelize,
-  DataTypes
+  Sequelize.DataTypes
 );
-const Assignee = require("./user/assignee.model.js")(sequelize, DataTypes); // New Assignee model
+const Assignee = require("./user/assignee.model.js")(sequelize, Sequelize.DataTypes); // New Assignee model
 
 // HERE ARE YOUR CURRENT ASSOCIATIONS
 
