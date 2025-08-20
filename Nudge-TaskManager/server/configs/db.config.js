55862606eaacb97b14a8f5a9f490930a345e620d
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2025-08-21 03:39:18 +0800

    fix: config

--- Nudge-TaskManager/server/configs/db.config.js
+++ Nudge-TaskManager/server/configs/db.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1234",
-  DB: "Nudge",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST || "127.0.0.1", // use IPv4 by default
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "1234",
+  DB: process.env.DB_NAME || "Nudge",
+  dialect: process.env.DB_DIALECT || "mysql",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
 };
